cb23614473256e758f770d7e95fe070b74854c08
Anand <anandvinoth@gmail.com>
2023-06-19 23:26:12 -0400

    master branch

---
+++ core-customize/hybris/bin/custom/tapetro/tapetrofacades/src/com/ta/petro/facades/constants/TapetroFacadesConstants.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package com.ta.petro.facades.constants;
+
+/**
+ * Global class for all TapetroFacades constants.
+ */
+public class TapetroFacadesConstants extends GeneratedTapetroFacadesConstants
+{
+	public static final String EXTENSIONNAME = "tapetrofacades";
+
+	private TapetroFacadesConstants()
+	{
+		//empty
+	}
+}
---
+++ core-customize/hybris/bin/custom/tapetro/tapetroinitialdata/src/com/ta/petro/initialdata/constants/TapetroInitialDataConstants.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package com.ta.petro.initialdata.constants;
+
+/**
+ * Global class for all TapetroInitialData constants.
+ */
+public final class TapetroInitialDataConstants extends GeneratedTapetroInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "tapetroinitialdata";
+
+	private TapetroInitialDataConstants()
+	{
+		//empty
+	}
+}
