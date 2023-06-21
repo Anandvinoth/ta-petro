d8fc3265e98aa67d94bba36ffc654fa04d61e037
Anandvinoth <anandvinoth@gmail.com>
2023-06-21 14:04:49 -0400

    src and test src was missing

---
+++ core-customize/hybris/bin/custom/tapetro/tapetrowebservices/src/com/ta/petro/core/queues/impl/OrderStatusUpdateQueue.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package com.ta.petro.core.queues.impl;
+
+import com.ta.petro.core.queues.data.OrderStatusUpdateElementData;
+
+
+/**
+ * Queue for {@link com.ta.petro.core.queues.data.OrderStatusUpdateElementData}
+ */
+public class OrderStatusUpdateQueue extends AbstractUpdateQueue<OrderStatusUpdateElementData>
+{
+	// EMPTY - just to instantiate bean
+}
